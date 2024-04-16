ddc27cdbda66340255aa97651b5b3757afecac31
marco9727 <marco.albarella97@gmail.com>
2024-04-16 10:25:09 +0200

    commit adesso la post per creare il libro funziona

--- createbook.php
+++ createbook.php
@@ -9,26 +9,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         exit;
     }
 
-    // Recupera i dati dal modulo
-    $titolo = $_POST['titolo'];
-    $autore = $_POST['autore'];
-    $anno_pubblicazione = $_POST['anno_pubblicazione'];
-    $genere = $_POST['genere'];
+    // Recupera i dati dal modulo e esegue una pulizia di base
+    $titolo = htmlspecialchars($_POST['titolo']);
+    $autore = htmlspecialchars($_POST['autore']);
+    $anno_pubblicazione = ($_POST['anno_pubblicazione']);
+    $genere = htmlspecialchars($_POST['genere']);
 
     // Verifica che l'anno di pubblicazione sia un numero positivo
-    if (!is_numeric($anno_pubblicazione) || $anno_pubblicazione < 0) {
+    if ($anno_pubblicazione <= 0) {
         echo "Si è verificato un errore: l'anno di pubblicazione non è valido.";
         exit;
     }
 
     // Prepara e esegui la query per l'inserimento di un nuovo libro
-    $stmt = $pdo->prepare("INSERT INTO libri (titolo, autore, anno_pubblicazione, genere) VALUES (:titolo, :autore, :anno_pubblicazione, :genere)");
-    $stmt->execute([
-        'titolo' => $titolo,
-        'autore' => $autore,
-        'anno_pubblicazione' => $anno_pubblicazione,
-        'genere' => $genere,
-    ]);
+    $stmt = $pdo->prepare("INSERT INTO libri (titolo, autore, anno_pubblicazione, genere) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$titolo, $autore, $anno_pubblicazione, $genere]);
 
     // Reindirizza l'utente alla home page dopo l'inserimento
     header("Location: /S1-L5/index.php");
@@ -38,12 +33,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 include __DIR__ . "/includes/initialHtml.php"
 ?>
 
-
 <div class="container">
     <h1 class="my-4">Aggiungi un nuovo libro</h1>
     
     <!-- Modulo per aggiungere un nuovo libro -->
-    <form action="/S1-L5/index.php" method="POST">
+    <form action="" method="POST">
         <div class="form-group">
             <label for="titolo">Titolo</label>
             <input type="text" class="form-control" id="titolo" name="titolo" required>
@@ -65,4 +59,5 @@ include __DIR__ . "/includes/initialHtml.php"
 </div>
 
 <?php 
-include __DIR__ . "/includes/endHtml.php"; ?>
+include __DIR__ . "/includes/endHtml.php";
+?>
