2fa3c65fe0e9708d03f491b5eda13a7c908bc0de
marco9727 <marco.albarella97@gmail.com>
2024-04-16 14:01:59 +0200

    commit aggiunta db

---
+++ gestione_libreria.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 16, 2024 alle 14:00
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gestione_libreria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `libri`
+--
+
+CREATE TABLE `libri` (
+  `id` int(11) NOT NULL,
+  `titolo` varchar(100) NOT NULL,
+  `autore` varchar(100) NOT NULL,
+  `anno_pubblicazione` int(11) NOT NULL,
+  `genere` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `libri`
+--
+
+INSERT INTO `libri` (`id`, `titolo`, `autore`, `anno_pubblicazione`, `genere`) VALUES
+(1, 'un animale selvaggio Joel Dicker', 'giovanni', 2024, 'horror'),
+(2, 'un animale selvaggio Joel Dicker', 'giovanni', 2024, 'horror'),
+(10, 'uno nessuno e centomila', 'pirandello ', 1999, 'narrativa');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `libri`
+--
+ALTER TABLE `libri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `libri`
+--
+ALTER TABLE `libri`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
